466b3293e6f4358e4beb31b89fa93a9ea7a3e0ae
AlkalineDreams <craig.dwyer@ocsbstudent.ca>
2016-11-14 14:40:04 -0500

    Update Mazifier.java

--- Mazifier.java
+++ Mazifier.java
@@ -81,89 +81,6 @@ public class Mazifier {
         	}
         	
         }
-package ass5test;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
-public class Mazifier {
-
-	public static void main(String[] args) throws IOException {
-		
-		FileReader fr = null;
-		String mazeFile = "Maze1.txt"; //CHANGE THIS CODE TO CHANGE WHICH MAZE YOU'RE SOLVING
-	    BufferedReader br = null;
-	    String line = "";
-	    
-	    fr = new FileReader(mazeFile);
-	    br = new BufferedReader(fr);
-	    
-	    line = br.readLine(); //reads first line of Maze1.txt
-        int i = (int) (line.length()); //array width should be the width of the first line
-        System.out.println(i); //for debugging
-        br.close();
-        
-        BufferedReader br2 = new BufferedReader(new FileReader(mazeFile));
-        int j = 0;
-        while (br2.readLine() != null) j++;
-        br2.close();
-        System.out.println(j);
-        
-		char[][] maze = new char[i][j]; //2d maze array creation
-		char[][] mazeSolved = maze;
-	    char[] lineArray = new char[line.length()];
-        
-	    BufferedReader br3 = new BufferedReader(new FileReader(mazeFile)); //new line reader so it starts at the beginning again
-
-	    //these nested loops populate the "maze[][]" array.
-        for(int x = 0; x < maze.length; x++) {
-        	
-                line = br3.readLine(); //selects next line
-                lineArray = line.toCharArray(); //turn line into char array
-                
-            for(int y = 0; y < maze[0].length; y++) {
-                maze[x][y] = lineArray[y]; //shoves line array into maze array at row[x]
-            }
-            
-        }
-        br3.close();
-        
-        //HOOOOOOOOOLY HELL I DID IT
-        //the above section took me forever because i kept getting weird errors
-        //but alas, i solved the problem, even if not in the most efficient way.
-        //anyway, we continue:
-        int[] startPosition = new int[2];
-        startPosition = FindStart(maze);
-        
-        if (startPosition[0] == -1) { //error handler
-        	System.out.println("Your maze needs to have a starting point marked by the letter 'S'.");
-        	System.out.println("Please run the program with a valid maze next time.");
-        } else {
-        	
-        	//this occurs when everything worked correctly, we have a valid starting position, and the maze is ready to be solved.
-        	System.out.println("Start Position: Maze Array at " + startPosition[0] + ", " + startPosition[1]); //debugging
-        	
-        	mazeSolved = FindPath(maze, maze, startPosition[0], startPosition[1]);
-        	
-        	if (mazeSolved == maze){ //if solution finder did nothing
-        		System.out.println("A solution was not found :(");
-        		
-        		for(int x = 0; x < mazeSolved.length; x++) {
-        			
-        			line = ""; //resets "line" variable
-        			
-        			for(int y = 0; y < mazeSolved[0].length; y++) {
-        				
-                        line = line + mazeSolved[x][y]; 
-                        
-                    }
-        			System.out.println(line);
-        		}
-        		
-        	}
-        	
-        }
 
 	}
 	
@@ -232,35 +149,6 @@ public class Mazifier {
 					return currentMaze;
 				}
 				else if (FindPath(currentMaze, originalMaze, x+1, y) != currentMaze){ //TEST EAST
-					solved = true;
-					return currentMaze;
-				}
-				else if (FindPath(currentMaze, originalMaze, x, y-1) != currentMaze){ //TEST SOUTH
-					solved = true;
-					return currentMaze;
-				}
-				else if (FindPath(currentMaze, originalMaze, x-1, y) != currentMaze){ //TEST WEST
-					solved = true;
-					return currentMaze;
-				}
-				else {
-					if (currentMaze[x][y] == 'S'){
-					} else {currentMaze[x][y] = '.';} //deletes item from solution path if it isn't the starting point
-					return currentMaze;
-				}
-			}
-			
-		} else {
-			
-			//when out of bounds
-			
-		}
-
-		return currentMaze;
-		
-	}
-
-}
 					solved = true;
 					return currentMaze;
 				}
